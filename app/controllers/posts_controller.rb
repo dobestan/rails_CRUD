c9f0149325dadccf4340a2e45d944f73ba5e7750
dobestan <dobestan@gmail.com>
2014-01-26 11:39:56 +0000

    complete NEW action and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,17 @@ class PostsController < ApplicationController
   def home
   end
 
+  #new -> create
   def new
   end
 
   def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.body = params[:body]
+    @post.save
+
+    redirect_to "/posts/new"
   end
 
   def show
