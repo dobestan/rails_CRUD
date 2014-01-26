5e7e179c1bd3a3bf31af8ffdab80aad4d3ef4037
dobestan <dobestan@gmail.com>
2014-01-26 11:54:47 +0000

    complete EDIT action and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,17 +13,25 @@ class PostsController < ApplicationController
     @post.body = params[:body]
     @post.save
 
-    redirect_to "/posts/new"
+    redirect_to "/posts/home"
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
+  #edit -> update
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.title = params[:title]
+    @post.body = params[:body]
+    @post.save
+
+    redirect_to "/posts/home"
   end
 
   def destroy
